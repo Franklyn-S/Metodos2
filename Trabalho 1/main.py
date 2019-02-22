98be1372b8366cc47810d2463527de1c7112c932
costadaniel <dancosta0601@gmail.com>
2019-02-21 21:29:21 -0300

    Import de derivadas

--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from argparse import ArgumentParser # Lib que eu vou usar pra fazer a passagem de argumentos (Daniel)
 from PIL import Image # Depois vou fazer o try/catch da importação dessa lib (Daniel)
+import derivate_functions as derivate
 
 # Abrindo Imagem no Formato 'Image'
 normal_image = Image.open('test-image.jpg')
