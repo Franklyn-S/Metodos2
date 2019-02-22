4b27a913f772ba75956c21c02da564de636b8e66
costadaniel <dancosta0601@gmail.com>
2019-02-21 21:19:26 -0300

    "Changes"

--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -7,7 +7,8 @@ from PIL import Image # Depois vou fazer o try/catch da importação dessa lib (
 normal_image = Image.open('test-image.jpg')
 
 # Criando Image em Grayscale
-grayscale_image = normal_image.convert('LA')
+grayscale_image = normal_image.convert('L')
 
 # Salvando a imagem em Grayscale em disco
-grayscale_image.save('test-image-grayscale.png')
+grayscale_image.save('test-image-grayscale.jpg')
+
