43b4967e235908ffc8b2f58fe837b8e4631d8629
jarelio <jareliofilho@gmail.com>
2019-02-21 23:11:11 -0300

    Adição dos loops e criação da imagem com a máscara aplicada

--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -18,4 +18,25 @@ def function(image, position):
     # Retorna valor do pixel
     return image.getpixel(position)
 
-print(function(grayscale_image, (0, 0)))
+# print(function(grayscale_image, (0, 0)))
+
+# Cria a imagem que receberá a mascara nova
+masc_image = grayscale_image
+
+# Recebe valor de altura e largura da imagem (último pixel da 'direita' e último pixel de 'baixo')
+width, height = masc_image.size
+
+#Trabalhando percorrendo da esquerda pra direita da imagem (percorrendo por colunas)
+
+# Laço de repetição para utilização do Forward (Todos os pixels da primeira coluna da imagem)
+for x in range(0, height):
+    masc_image.putpixel( (x,0) , derivate.forward_derivation_image(grayscale_image, (x,0), 'y') )
+
+# Laço de repetição para utilização do Central (Todos os pixels das colunas e linhas centrais)
+for x in range(0, height-1):
+    for y in range(0, width-1):
+        masc_image.putpixel( (x,y) , derivate.central_derivation_image(grayscale_image, (x,y), 'y') )
+
+# Laço de repetição para utilização do Backward (Todos os pixels da última coluna da imagem)
+for x in range(0, height):
+    masc_image.putpixel( (x,width) , derivate.backward_derivation_image(grayscale_image, (x,width), 'y'))
