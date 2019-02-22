b20945790415a0425e64d190f0af627a5f35f1d1
Franklyn Seabra <franklynseabra12@hotmail.com>
2019-02-22 11:40:08 -0300

    central roda, mas a imagem ainda não mostra só as bordas

--- Trabalho 1/derivate_functions.py
+++ Trabalho 1/derivate_functions.py
@@ -9,47 +9,36 @@ def central_derivation(function, value, delta):
 
 def forward_derivation_image(image, position, direction):
     # Pega o valor de f(x)
-    #print(image.size)
-    #print(position)
+    
     f_x = image.getpixel(position)
 
     # Pega o valor de f(x + 1)
     if(direction == 'x'):
         # Caso esteja percorrendo em x
-        new_position = list(position)
-        new_position[1] += 1
-        delta_position = tuple(new_position)
+        delta_position = (position[0], position[1]+1)
         
         f_x_delta = image.getpixel(delta_position)
     elif(direction == 'y'):
         # Caso esteja percorrendo em y
-        new_position = list(position)
-        new_position[0] += 1
-        delta_position = tuple(new_position)
-        
+        delta_position = (position[0]+1, position[1])
         f_x_delta = image.getpixel(delta_position)
     
     return abs(f_x_delta - f_x)
 
 def backward_derivation_image(image, position, direction):
     # Pega o valor de f(x)
-    #print(image.size)
-    print(position)
+    
     f_x = image.getpixel(position)
 
     # Pega o valor de f(x - 1)
     if(direction == 'x'):
         # Caso esteja percorrendo em x
-        new_position = list(position)
-        new_position[1] -= 1
-        delta_position = tuple(new_position)
-        
+        delta_position = (position[0], position[1]-1)
+
         f_x_delta = image.getpixel(delta_position)
     elif(direction == 'y'):
         # Caso esteja percorrendo em y
-        new_position = list(position)
-        new_position[0] -= 1
-        delta_position = tuple(new_position)
+        delta_position = (position[0]-1, position[1])
         
         f_x_delta = image.getpixel(delta_position)
     
@@ -58,23 +47,17 @@ def backward_derivation_image(image, position, direction):
 def central_derivation_image(image, position, direction):
     if(direction == 'x'):
         # Caso esteja percorrendo em x
-        new_position = list(position)
-        new_position[1] += 1
-        delta_position = tuple(new_position)
+        delta_position = (position[0], position[1] +1)
         f_x_delta_forward = image.getpixel(delta_position)
         
-        new_position[1] -= 2
-        delta_position = tuple(new_position)
+        delta_position = (position[0], position[1]-1)
         f_x_delta_backward = image.getpixel(delta_position)
     elif(direction == 'y'):
         # Caso esteja percorrendo em y
-        new_position = list(position)
-        new_position[0] += 1
-        delta_position = tuple(new_position)
+        delta_position = (position[0]+1, position[1])
         f_x_delta_forward = image.getpixel(delta_position)
         
-        new_position[0] -= 2
-        delta_position = tuple(new_position)
+        delta_position = (position[0]-1, position[1])
         f_x_delta_backward = image.getpixel(delta_position)
     
-    return abs((f_x_delta_forward - f_x_delta_backward)/2)
+    return int(abs((f_x_delta_forward - f_x_delta_backward)/2))
--- Trabalho 1/main.py
+++ Trabalho 1/main.py
@@ -6,7 +6,6 @@ import derivate_functions as derivate
 
 # Abrindo Imagem no Formato 'Image'
 normal_image = Image.open('test-image.jpg')
-
 # Criando Image em Grayscale
 grayscale_image = normal_image.convert('L')
 
@@ -18,7 +17,7 @@ def function(image, position):
     # Retorna valor do pixel
     return image.getpixel(position)
 
-# print(function(grayscale_image, (0, 0)))
+#print(function(grayscale_image, (0, 0)))
 
 # Cria a imagem que receberá a mascara nova
 masc_image = grayscale_image
@@ -26,19 +25,23 @@ masc_image = grayscale_image
 # Recebe valor de altura e largura da imagem (último pixel da 'direita' e último pixel de 'baixo')
 
 width, height = masc_image.size
+#print(width, height)
 
 #Trabalhando percorrendo da esquerda pra direita da imagem (percorrendo por colunas)
 
 # Laço de repetição para utilização do Forward (Todos os pixels da primeira coluna da imagem)
 for x in range(0, height-1):
-    masc_image.putpixel(tuple([0,x]), derivate.forward_derivation_image(grayscale_image, tuple([0,x]), 'x') )
+    masc_image.putpixel( (0,x), derivate.forward_derivation_image(grayscale_image, (0,x), 'x') )
 
 # Laço de repetição para utilização do Central (Todos os pixels das colunas e linhas centrais)
-for x in range(0, height):
-    for y in range(0, width-1):
-        masc_image.putpixel(tuple([y,x]), derivate.central_derivation_image(grayscale_image, tuple([y,x]), 'x') )
+for x in range(0, height-1):
+    for y in range(1, width-2):
+        value = derivate.central_derivation_image(grayscale_image, (y,x), 'x')
+        masc_image.putpixel( (y,x),  value)
+
 # Laço de repetição para utilização do Backward (Todos os pixels da última coluna da imagem)
 for x in range(0, height):
-    masc_image.putpixel( tuple([width-1,x]) , derivate.backward_derivation_image(grayscale_image, tuple([width-1,x]), 'x') )
+    masc_image.putpixel( (width-1,x) , derivate.backward_derivation_image(grayscale_image, (width-1,x), 'x') )
 
 masc_image.save('masc-image.jpg')
+masc_image.show()
