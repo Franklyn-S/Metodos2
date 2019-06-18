c50fc29c6964ee4ed8e66eb150f04d800e181964
paulosgaf <paulosgaf@hotmail.com>
2019-06-18 16:15:15 -0300

    Delete householder.h

--- HouseHolder com Eigen/householder.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOUSEHOLDER_H
-#define HOUSEHOLDER_H
-
-Eigen::VectorXd zeros(int size);
-Eigen::MatrixXd mountHouseHolder(Eigen::Ref<Eigen::MatrixXd> A, int c);
-tuple<Eigen::MatrixXd, Eigen::MatrixXd> HouseHolder(Eigen::Ref<Eigen::MatrixXd> A);
-
-
-#endif
